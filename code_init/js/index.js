d7ba4e585f8956c510c3950af178e87391750254
Eduardo Vargas <eduant.varmau@gmail.com>
2022-02-28 09:29:21 -0600

    Se comento el JSS y se corrio las funcionalidades del mismo, ahohora ya es posible calcular el total y borrar prodcutos dadod por un usuario

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,5 +1,5 @@
 // ITERATION 1
-
+// A TRAVEZ DE ESTA FUNCIÓN SE TOMAN LOS DATOS PARA CALCULAR EL TOTAL POR CADA PRODUCTO E IMPRIMIRLO
 function updateSubtotal(product) {
 
   console.log('Calculating subtotal, yey!');
@@ -16,10 +16,13 @@ function updateSubtotal(product) {
   return total
   }
   
-    // ITERATION 2
+// ITERATION 2
+// A TRAVEZ DE ESTA FUNCIÓN  RECORRE EL ARREGLO DONDE SE ENCUENTRAN LOS PRODUCTOS Y POR CADA UNO
+// MANDA A LLAMAR A LA FUNCIÓN "updateSubtotal" POR MEDIO DE ELLA RECIBE EL TOTAL DE CADA PRODUCTO
+// Y LO ACUMULA EN UNA VARIABLE. IMPRIME EL TOTAL DE LA COMPRA DE TODOS LOS PRODUCTOS
   
   function calculateAll(product) {
-    const elementProduct = document.getElementsByClassName(`product`)
+    //const elementProduct = document.getElementsByClassName(`product`) //SE QUITA
     //console.log(elementProduct)
 
     let totalTotal = 0;
@@ -38,26 +41,30 @@ function updateSubtotal(product) {
      totalTotal += updateSubtotal(product);
  });
 
-    // ITERATION 3
-    let grandTotal = document.querySelector(`#total-value span`).innerHTML;
+// ITERATION 3
+
+    let  totalGlobal = document.getElementById("totalGlobal"); // SE AGREGA
+    //let grandTotal = document.querySelector(`#total-value span`).innerHTML; // SE QUITA
     
-    const collection = document.querySelectorAll(`.product`);
+    // const collection = document.querySelectorAll(`.product`);
   
-    let totalArr = Array.from(collection)
+    // let totalArr = Array.from(collection)
   
-    totalArr = totalArr.reduce(function (acc,product) { 
-      return acc += updateSubtotal(product)}, 0);
-    console.log(totalArr);
+    // totalArr = totalArr.reduce(function (acc,product) { 
+    //   return acc += updateSubtotal(product)}, 0);
+    // console.log(totalArr);
 
-    grandTotal = Number(totalArr).toFixed(2);
+    // grandTotal = Number(totalArr).toFixed(2); // SE QUITA
 
-    return grandTotal;
+    totalGlobal.textContent = totalTotal //SE AGREGA
+    return totalTotal;
     }
     
     
   
   // ITERATION 4
-
+  // A TRAVEZ DE ESTA FUNCIÓN SE ELIMINAN PRODUCTOS POR MEDIO DE LA ELIMINACIÓN DE NODO.
+  // UNA VEZ ELIMIANDO EL PRODUCTO SE VUELVE A CALCULAR LOS PRECIOS
 
   function removeProduct(event) {
  
@@ -68,7 +75,10 @@ function updateSubtotal(product) {
     calculateAll();
   }
   
-  // ITERATION 5
+// ITERATION 5
+// A TRAVEZ DE ESTA FUNCIÓN SE CREA UN NUEVO PRODUCTO, ES AÑADIDO POR EL BOTON
+// ADQUIERE TODAS LAS DUNCIONALIDADES  Y SE AÑADE COMO UN HIJO DEL NODO QUE ENGLOBA LA TABLA
+
   
   function createProduct() {
     const mainProductsTable = document.querySelector(`tbody`);
@@ -96,16 +106,19 @@ function updateSubtotal(product) {
     newProductItem.classList.add('product');
     mainProductsTable.appendChild(newProductItem);
   
-    newProductItem.querySelector(`.btn-remove`).addEventListener(`click`, removeProduct);
+    newProductItem.querySelector(`.btn-delete`).addEventListener(`click`, removeProduct); //SE COLOCO LA CLASE CORRECTA DEL BOTÓN
   }
   
+
+// TOTDOS LOS BOTONES SE LES AÑADE UN "addEventListener" 
+// AQUI ESTAN LA FUNCION DE LOS BOTONES
   window.addEventListener('load', () => {
 
     const calculatePricesBtn = document.getElementById('calc');
 
     calculatePricesBtn.addEventListener('click', calculateAll);
   
-    const removeButton = document.querySelectorAll(`.btn-delete`);
+    let removeButton = document.querySelectorAll(`.btn-delete`);// SE QUITA CONST
     
     removeButton.forEach(button => {
       
